f601a8663e12982a056d20091cf6670e0b7669c2
Jader Santos <jader.santos@portx.io>
2025-07-09 18:35:51 -0300

    Add ImageDetectionController with /image-to-text endpoint for image description

---
+++ src/main/java/dev/jaderss/workshop/multimodal/ImageDetectionController.java
@@ -0,0 +1,29 @@
+package dev.jaderss.workshop.multimodal;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.util.MimeTypeUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class ImageDetectionController {
+
+    @Value("classpath:images/34312498-1caf-41f3-a9c6-4f80e9a3f53a-1400.jpg")
+    private Resource image;
+
+    private final ChatClient chatClient;
+
+    @GetMapping("/image-to-text")
+    public String image() {
+        return chatClient.prompt()
+                .user(u -> u.text("Describe the image you see in this sample.")
+                        .media(MimeTypeUtils.IMAGE_JPEG, image))
+                .call()
+                .content();
+    }
+
+}
